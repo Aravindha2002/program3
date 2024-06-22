5cfccbe242b3edf8e73ea269e76292929f1bb56b
Aravindha2002 <23203001@srcas.ac.in>
2024-06-22 11:55:46 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello{
+public static void main(String[]args){
+System.out.println("welcome");
+System.out.println("to");
+System.out.println("devops");
+System.out.println("class");
+}
+}
