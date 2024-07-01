dd41f3c2ed53cf8fbb55de65e91b056850733164
Aravindha2002 <23203001@srcas.ac.in>
2024-07-01 11:59:30 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,10 @@
+class sample{
+public static void main(string[]args}
+{
+system.out.println("hello")
+system.out.println"everyone")
+system.out.println("wellcome")
+system.out.println("to")
+system.out.println("m.sc")
+}
+}
